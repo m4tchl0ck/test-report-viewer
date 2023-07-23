025c10f1b20525c66e273794441cb13fda6e31ac
Adrian <m4tchl0ck@outlook.com>
2023-07-23 22:00:33 +0200

    Install GraphQL client

--- angular/test-report-viewer/README.md
+++ angular/test-report-viewer/README.md
@@ -25,3 +25,15 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+
+## Settings
+
+Create file `src\.env.ts` and define class with variables
+
+```typescript
+export class Env {
+    public static get GraphQLURL(): string 
+    { return ''; }
+}
+```
--- angular/test-report-viewer/src/app/app.module.ts
+++ angular/test-report-viewer/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestExecutionsListComponent } from './test-executions-list/test-executions-list.component';
+import { GraphQLModule } from './graphql.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { TestExecutionsListComponent } from './test-executions-list/test-executi
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GraphQLModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
